7366609b3e4ee15e2909ea74b8a75f8464ec355c
fsddemo2 <gitdemo2@visualcon.net>
2015-09-13 12:00:09 -0600

    Primeras 2 implementaciones

---
+++ fibo.py
@@ -0,0 +1,18 @@
+def fibonacci_iter(n):
+ """ Fibonacci iterativo """
+
+ a,b = 1,1
+ for i in range(n-1):
+  a,b = b,a+b
+ return a
+
+def fibonacci_rec(n):
+ """ Fibonacci recursivo """
+ if n==1 or n==2:
+  return 1
+ return fibonacci_rec(n-1)+fibonacci_rec(n-2)
+
+
+if __name__ == "__main__":
+    print fibonacci_iter(5)
+    print fibonacci_rec(6)
